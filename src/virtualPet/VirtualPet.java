0e27f804f7f815c0cb885e593ee3e22f1e584121
mosguinz <mos.guinz@gmail.com>
2020-11-24 18:27:13 -0800

    Add headers

--- src/virtualPet/VirtualPet.java
+++ src/virtualPet/VirtualPet.java
@@ -1,4 +1,15 @@
+/*
+ * Assignment 09
+ * Description: [Part 1.2 of 2] VirtualPet class
+ * Name: Kullathon "Mos" Sitthisarnwattanachai
+ * Teammate: Amber Hartigan
+ * ID: 921425216
+ * Class: CSC 210-03
+ * Semester: Fall 2020
+ */
+
 package virtualPet;
 
 public class VirtualPet {
+
 }
--- src/virtualPet/VirtualPetTester.java
+++ src/virtualPet/VirtualPetTester.java
@@ -1,3 +1,13 @@
+/*
+ * Assignment 09
+ * Description: [Part 1.1 of 2] VirtualPetTester class
+ * Name: Kullathon "Mos" Sitthisarnwattanachai
+ * Teammate: Amber Hartigan
+ * ID: 921425216
+ * Class: CSC 210-03
+ * Semester: Fall 2020
+ */
+
 package virtualPet;
 
 public class VirtualPetTester {
