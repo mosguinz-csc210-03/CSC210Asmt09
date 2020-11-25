aea67d44a159fd8c32faf11efd8addd399cd5065
mosguinz <mos.guinz@gmail.com>
2020-11-24 19:02:34 -0800

    Add requirements for (1)(2)(i) thru (ii)

--- src/virtualPet/VirtualPet.java
+++ src/virtualPet/VirtualPet.java
@@ -12,4 +12,42 @@ package virtualPet;
 
 public class VirtualPet {
 
+    private static final int MAX_NAME_LENGTH = 30;
+    /* Part (1)(2)(i) */
+    private String name;
+    private int happiness;
+    private int energy;
+
+    public VirtualPet() {
+        this.name = "DEFAULT";
+        setStartingValues();
+    }
+
+    public VirtualPet(String name) {
+        setName(name);
+        setStartingValues();
+    }
+
+    public void setHappiness(int happiness) {
+        this.happiness = happiness;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public void setName(String name) {
+        /* Note that (1)(2)(ii) specifies a limit that is exclusive of 30. */
+        if (name.length() >= MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(String.format("Name must less than %d characters in length", MAX_NAME_LENGTH));
+        }
+        this.name = name;
+    }
+
+    /**
+     * Set the starting values for the instance. Supplemental to (1)(2)(ii).
+     */
+    private void setStartingValues() {
+        happiness = energy = 25;
+    }
 }
