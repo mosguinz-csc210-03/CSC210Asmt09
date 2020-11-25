3688a5a25711de27b59b3b1338695c9eded0d501
mosguinz <mos.guinz@gmail.com>
2020-11-24 19:33:19 -0800

    Add method to prompt and validate pet amount

--- src/virtualPet/VirtualPetTester.java
+++ src/virtualPet/VirtualPetTester.java
@@ -10,5 +10,31 @@
 
 package virtualPet;
 
+import java.util.Scanner;
+
 public class VirtualPetTester {
+
+    public static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        promptPetAmount();
+    }
+
+    /**
+     * Prompt user for amount of pets to create. (1)(1)(a)-(b).
+     *
+     * @return A valid amount of pets to create.
+     */
+    private static int promptPetAmount() {
+        while (true) {
+            System.out.print("Enter the amount of virtual pets to create: ");
+            int amt = scan.nextInt();
+            if (amt > 0) {
+                System.out.println("Invalid amount. Must be at least one.");
+                continue;
+            }
+            scan.nextLine();
+            return amt;
+        }
+    }
 }
